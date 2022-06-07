87e2156d95107259aa0e55cd3f30c34c33ef4d0d
wgumenyuk <wlad@gumenyuk.de>
2022-06-07 21:08:04 +0200

    Konstanten hinzugefügt

---
+++ src/constants.ts
@@ -0,0 +1,30 @@
+/**
+    Log-Level für den Logger.
+*/
+const levels = {
+    DBG: 1,
+    INFO: 2,
+    WARN: 3,
+    ERR: 4,
+    FTL: 5
+};
+
+/**
+    Farben für den Logger.
+*/
+const colors = {
+    RESET: "\x1b[0m",
+    GRAY: "\x1b[90m",
+    RED: "\x1b[91m",
+    RED_BG: "\x1b[41;1m",
+    GREEN: "\x1b[92m",
+    YELLOW: "\x1b[93m",
+    MAGENTA: "\x1b[95m",
+    CYAN: "\x1b[96m",
+    WHITE: "\x1b[97m"
+};
+
+export {
+    levels,
+    colors
+};
