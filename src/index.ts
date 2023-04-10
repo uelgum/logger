67e346b596f016799f66ee93cd3713c1cac02619
wgumenyuk <wlad@gumenyuk.de>
2023-04-10 03:21:54 +0200

    ✨ feat: export types

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import Logger from "./logger";
 import Adapter from "./adapter";
 import ConsoleAdapter from "./adapters/console";
 
+// Types
+import type { Level } from "./levels";
+import type { Log, Options } from "./logger";
+
 export {
     Adapter,
     ConsoleAdapter
+};
+
+export type {
+    Options,
+    Level,
+    Log
 };
 
 export default Logger;
