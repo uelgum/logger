d2aa0d752baea5d0a76bdc6fdc591dccdf62a216
wgumenyuk <wlad@gumenyuk.de>
2023-06-01 11:11:38 +0200

    feat: add index

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Logger, Level } from "./logger";
+export { ConsoleAdapter } from "./adapters/console";
+export { FileAdapater } from "./adapters/file";
+
+// Types
+export type { Log, Adapter } from "./logger";
